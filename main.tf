3beda1af774c8ede89289ff14f4848497cf48291
nizarakbarm <nizar@domainesia.com>
2023-10-18 14:44:48 +0700

    feat(OIDC): :sparkles: Add aws provider to terraform.tf and resource terraform_cloud_oidc for creating oidc of terraform cloud

---
+++ main.tf
@@ -0,0 +1,6 @@
+// Create OIDC
+resource "aws_iam_openid_connect_provider" "terraform_cloud_oidc" {
+    url = "https://app.terraform.io"
+    client_id_list = [ "aws.workload.identity" ]
+    thumbprint_list = [ "9e99a48a9960b14926bb7f3b02e22da2b0ab7280" ]
+}
